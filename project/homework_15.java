afe5cf49732ace82ff99e715f103e2afb44b5ef4
Matthew <maslo200408@gmail.com>
2022-12-02 17:47:34 +0300

    my homework

---
+++ project/homework_15.java
@@ -0,0 +1,29 @@
+public class homework_15 <F, S, T> {
+    private F first;
+    private S second;
+    private T third;
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    public T getThird() {
+        return third;
+    }
+
+    public void setThird(T third) {
+        this.third = third;
+    }
+}
